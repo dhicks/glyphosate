e174baeeb4c97b680d40bc380e502f270c112af7
Dan Hicks <hicks.daniel.j@gmail.com>
2017-04-17 21:02:12 -0400

    documentation to prep_fit_word2vec

--- 3_prep_fit_word2vec.R
+++ 3_prep_fit_word2vec.R
@@ -31,18 +31,49 @@ library(tidytext)
 tokens_df = comments %>% 
 	select(-attachment_urls) %>%
 	unnest_tokens(token, comment_text)
-## Save this for use in the clustering script
-save(tokens_df, file = 'tokens_df.Rdata')
 
 ## Generate files of ngrams
 tokens_df %>%
 	.$token %>%
 	cat(file = outfiles[1])
-## Combine bigrams and trigrams
+## Combine bigrams, trigrams, and quadgrams
 word2phrase(outfiles[1], outfiles[2], force = TRUE)
 word2phrase(outfiles[2], outfiles[3], force = TRUE)
 
-## Train the model
+#' The blocks above construct ngram-ed text files suitable for processing by `train_word2vec`.  Next we ngram-ize `tokens_df`.  We first read back in the final file and extract its vocabulary. 
+ngramed = read_file(outfiles[3])
+vocabulary = ngramed %>%
+	str_split(' ') %>% 
+	## str_split returns a list
+	unlist() %>%
+	unique() %>% 
+	sort()
+#' Next we construct all of the bigrams, trigrams, and quadgrams in tokens_df. We then filter these based on which tokens appear in the vocabulary.  
+#' NB A limitation of the particular approach below is that we don't remove overlapping ngrams.  For example, the first comment contains 'our,' 'our_food,' 'our_food_supply,' 'food,' 'food_supply,' and 'supply,' rather than just 'our_food_supply.'  
+tokens_df %>%
+	group_by(comment_id) %>%
+	mutate(token.idx = row_number()) %>% 
+	ungroup() %>%
+	mutate(token_p1 = lead(token), 
+		   bigram = str_c(token, token_p1, sep = '_'), 
+		   token_p2 = lead(token_p1), 
+		   trigram = str_c(bigram, token_p2, sep = '_'), 
+		   token_p3 = lead(token_p2), 
+		   quadgram = str_c(trigram, token_p3, sep = '_')) %>% 
+	select(-starts_with('token_')) %>% 
+	gather(key = ngram, value = token, 
+		   token:quadgram, -token.idx) %>% 
+	filter(token %in% vocabulary) %>% 
+	filter(!duplicated(.)) %>%
+	arrange(comment_id, token.idx) -> tokens_df
+tokens_df %>%
+	filter(str_detect(comment_id, '0002')) %>%
+	select(comment_id, valence, token) %>%
+	head(20)
+## Save this for use in later scripts
+save(tokens_df, file = 'tokens_df.Rdata')
+
+#' Now we train the word2vec model. (Or, since training can take several minutes, we detect that the file already exists and skip training.)
 if (!file.exists(outfiles[4])) {
 	system.time(
 		model <- train_word2vec(outfiles[3],
